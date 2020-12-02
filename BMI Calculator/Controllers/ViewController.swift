f6ec38315dcd8864a239a7dc7eeca6e083727f87
Faiq Ali <faiqueali017@gmail.com>
2020-12-02 21:10:06 +0500

    Make IBAction of height and weight slider

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        print(String(format: "%.2f", sender.value))
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
 }
 
