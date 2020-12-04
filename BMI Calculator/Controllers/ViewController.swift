2fd87fb233c2a69a61a5ed50e637272cec379e79
Faiq Ali <faiqueali017@gmail.com>
2020-12-04 19:27:29 +0500

    Make IBOutlets of slider height and weight and also make its calculate button

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
+    //make weight & height slider outlets to get access to their current selected values
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,8 +30,17 @@ class ViewController: UIViewController {
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-        let weight = String(Int(sender.value))
+        let weight = String(format: "%.0f", sender.value)
         weightLabel.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height, 2)
+        
+        print(bmi)
     }
     
 }
