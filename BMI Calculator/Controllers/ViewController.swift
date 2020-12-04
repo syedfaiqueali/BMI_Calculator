ca87ca7f2726538bc7241bd0f5a696b8525899a3
Faiq Ali <faiqueali017@gmail.com>
2020-12-04 19:47:06 +0500

    Create UI Outlets programmatically

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -39,8 +39,13 @@ class ViewController: UIViewController {
         let weight = weightSlider.value
         
         let bmi = weight / pow(height, 2)
+        //print(bmi)
         
-        print(bmi)
+        //For segue
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        self.present(secondVC, animated: true, completion: nil)
+        
     }
     
 }
