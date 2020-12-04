bed7b8512e7439fdb86f16666c10a36aa4f4e864
Faiq Ali <faiqueali017@gmail.com>
2020-12-04 21:32:12 +0500

    Handling of optional value of bmi

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -10,18 +10,32 @@ import Foundation
 
 struct CalculatorBrain {
     
-    //Variables
-    var bmi: Float = 0.0
+    /**Variables
+     Initialization in struct is neccessary because if we don't do it the in CalculateViewController class when where we were creating it's instance having to set the value.
+     
+     But in reality it doesn't makes sense that initializing the value.
+     var bmi: Float = 0.0
+     
+     So it better to convert them into optional type and remove the value*/
     
+    var bmi: Float?
     
     //MARK:- Helper Methods
     mutating func calculateBMI(height: Float, weight: Float) {
         bmi = weight / (height * height)
-        
     }
     
     func getBMIValue() -> String {
-        let bmiTo1DecimalPlace = String(format: "%.1f", bmi)
+        /*
+        if let safeBMI = bmi {
+            let bmiTo1DecimalPlace = String(format: "%.1f", safeBMI)
+            return bmiTo1DecimalPlace
+        } else {
+            return "0.0"
+        }
+ */
+        //Nil coalescing 
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi ?? 0.0)
         return bmiTo1DecimalPlace
     }
     
